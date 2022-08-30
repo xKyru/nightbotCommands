fa596d74d1f333992ca6d332099b19f5096b582f
CarlosMedinaC <camc-107@hotmail.com>
2022-08-30 14:05:06 -0500

    Command v0.1

--- shoutout.js
+++ shoutout.js
@@ -1,10 +1,24 @@
 // !addcom !so $(eval let usuario='$(query)'; let usuarioTwitch='$(twitch $(query) "{{displayName}}")'; let usuarioUrl='$(twitch $(query) {{url}})'; $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
-// !addcom !so $(eval let usuario='$(query)'; let usuarioTwitch='$(twitch $(query))'; $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
+// !addcom !so $(eval let usuario="$(query)"; var usuarioTwitch="$(twitch usuario)"; $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
 
 
-if(!usuario){
-    "Error en el comando: !so [canal]";
+//!addcom !so $(eval var usuario="$(query)".toLowerCase(); var usuarioTwitch="$(twitch $(usuario))"; $(urlfetch https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
+
+let announcements = [
+    "announce",
+    "announcepurple",
+    "announcegreen",
+    "announceorange"
+]
+
+if(!userName){
+    "Error en el comando: !so [userName]";
 }else{
-    `/announcepurple${usuarioTwitch}`;
+    var usuarioTwitch='$(twitch $(userName) "{{displayName}}")'
+    for(announce in announcements){
+        `/${announce} ${usuarioTwitch}`;
+        setTimeout(() => {
+            
+        }, 5100);
+    }
 }
-
