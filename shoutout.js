c2b6f8bc24ade523fa29687fa4c2f3716b5eaf70
CarlosMedinaC <camc-107@hotmail.com>
2022-08-30 14:13:47 -0500

    Command v0.04

--- shoutout.js
+++ shoutout.js
@@ -1,4 +1,3 @@
-// !addcom !so $(eval let usuario='$(query)'; let usuarioTwitch='$(twitch $(query) "{{displayName}}")'; let usuarioUrl='$(twitch $(query) {{url}})'; $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
 // !addcom !so $(eval let usuario="$(query)"; var usuarioTwitch="$(twitch usuario)"; $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
 
 
