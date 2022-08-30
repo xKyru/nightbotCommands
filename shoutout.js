0fc2d617847020da4cff8b7cc6712876b7e231e5
CarlosMedinaC <camc-107@hotmail.com>
2022-08-30 10:50:11 -0500

    Command v8

--- shoutout.js
+++ shoutout.js
@@ -1,10 +1,10 @@
-// !addcom !so $(eval let usuario='$(query)'; let usuarioTwitch='$(twitch $(query) {{displayName}})'; let usuarioUrl='$(twitch $(query) {{url}})'; $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
+// !addcom !so $(eval let usuario='$(query)'; let usuarioTwitch='$(twitch $(query) "{{displayName}}")'; let usuarioUrl='$(twitch $(query) {{url}})'; $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
+// !addcom !so $(eval let usuario='$(query)'; let usuarioTwitch='$(twitch $(query))'; $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/shoutout.js))
+
 
 if(!usuario){
     "Error en el comando: !so [canal]";
-}else if(!usuarioTwitch || !usuarioUrl){
-    "Usuario no encontrado";
 }else{
-    `/announcepurple Sigue a ${usuarioTwitch} en su canal: ${usuarioUrl}`;
+    `/announcepurple${usuarioTwitch}`;
 }
 
