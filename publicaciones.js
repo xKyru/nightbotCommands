da003cdf7ce6101a74a85ee072ae23902a9a7dc7
xKyru <camc-107@hotmail.com>
2022-10-23 18:27:00 -0500

    test com 1.0

--- publicaciones.js
+++ publicaciones.js
@@ -4,23 +4,23 @@ function announcements(announce) {
 	/* URL posts */
 	const posts = {
 		instapic: {
-			msg: "📷NUEVA FOTO📷 Me ayudas mucho al darle like, comentar y compartir",
+			msg: "📷NUEVA FOTO📷 Me ayudas mucho al darle like, comentar y compartir 💜",
 			url: "https://www.instagram.com/p/Cj4Ns0RjgZc"
 		},
 		instareel: {
-			msg: "👀NUEVO REEL👀 Me ayudas mucho al darle like, comentar y compartir",
+			msg: "👀NUEVO REEL👀 Me ayudas mucho al darle like, comentar y compartir 💜",
 			url: "https://www.instagram.com/p/Cj4Ns0RjgZc"
 		},
 		youtube: {
-			msg: "📽️NUEVO VIDEO📽️ Me ayudas mucho al darle like, comentar y compartir",
+			msg: "📽️NUEVO VIDEO📽️ Me ayudas mucho al darle like, comentar y compartir 💜",
 			url: "https://www.youtube.com/watch?v=dUcjHlO7LjQ"
 		},
 		tiktok: {
-			msg: "🔥NUEVO TIKTOK🔥 Me ayudas mucho al darle like, comentar y compartir 💜💜💜",
+			msg: "🔥NUEVO TIKTOK🔥 Me ayudas mucho al darle like, comentar y compartir 💜",
 			url: "https://www.tiktok.com/@eritrajinx/video/7157397576126352646"
 		},
 		sorteo: {
-			msg: "✨SORTEO ION BUNDLE 2.0✨ Participa en el sorteo de skins Ion 2.0 #Valorant",
+			msg: "✨SORTEO ION BUNDLE 2.0✨ Participa en el sorteo de skins Ion 2.0 #Valorant 💜",
 			url: "https://twitter.com/EritraJinx/status/1582832735776710656"
 		},
 		sorteo2: {
@@ -33,11 +33,11 @@ function announcements(announce) {
 		}
 	};
 
-	if(announce.toLowerCase() in posts){
-		return `/announce $(posts[announce.toLowerCase()].msg) $(posts[announce.toLowerCase()].url)`;
+	if(announce in posts){
+		return `/announce $(posts[announce].msg) $(posts[announce].url)`;
 	}else{
 		return "Opción no encontrada.";
 	};
 }
 
-/* (eval type = decodeURIComponent(`$(querystring)`); $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/publicaciones.js)) */
+/* $(eval const type = decodeURIComponent(`$(querystring)`); $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/publicaciones.js)) */
