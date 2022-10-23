ecd9fd1651b1b9c6269cfb6897447be2703fcf54
xKyru <camc-107@hotmail.com>
2022-10-23 16:53:08 -0500

    Commands updated
    updated commands

---
+++ publicaciones.js
@@ -0,0 +1,19 @@
+$(
+	eval let opt = '$(1)');  
+	switch (opt){
+		case twitter:
+			return "Twitter";
+		case facebook:
+			return "Facebook";
+		case tiktok:
+			return "TikTok";
+		case instagram:
+			return "Instagram";
+		case youtube:
+			return "Youtube";
+		case sorteo:
+			return "Sorteo";
+		default:
+			return "Opción no encontrada";
+	}
+)
--- shoutout.js
+++
@@ -1,5 +0,0 @@
-$(eval if ($(1) && $(2)) {
-	Math.floor(Math.random() * ($(2) - $(1) + 1) + $(1));
-} else {
-	Math.floor((Math.random() * 100) + 1);
-})
