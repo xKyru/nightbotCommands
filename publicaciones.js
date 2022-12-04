cdce7b48ad6febee11aa5538a38a3cc4250c4270
xKyru <camc-107@hotmail.com>
2022-12-04 01:32:37 -0600

    updated 04-12-22

--- publicaciones.js
+++ publicaciones.js
@@ -21,7 +21,7 @@ function announcements(announce) {
 		tiktok: {
 			title: "🔥𝐍𝐔𝐄𝐕𝐎 𝐓𝐈𝐊𝐓𝐎𝐊🔥",
 			msg: "Me ayudas mucho al darle like, comentar y compartir 💜",
-			url: "https://www.tiktok.com/@eritrajinx/video/7161153417497382149"
+			url: "https://www.tiktok.com/@eritrajinx/video/7172317138420796677"
 		},
 		twitter: {
 			title: "✨𝐒𝐎𝐑𝐓𝐄𝐎 𝐂𝐎𝐍𝐒𝐎𝐋𝐀 𝐃𝐄 𝐀𝐔𝐃𝐈𝐎 𝐙𝐆𝟎𝟏 𝐘𝐀𝐌𝐀𝐇𝐀✨",
