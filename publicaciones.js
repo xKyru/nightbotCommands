2a187a889bcd69173670aedfc7a574bcea408487
xKyru <camc-107@hotmail.com>
2023-02-28 22:16:28 -0600

    updated 28-08-23

--- publicaciones.js
+++ publicaciones.js
@@ -23,7 +23,7 @@ function announcements(announce) {
 		tiktok: {
 			title: "🔥𝐍𝐔𝐄𝐕𝐎 𝐓𝐈𝐊𝐓𝐎𝐊🔥",
 			msg: "Me ayudas mucho al darle like, comentar y compartir 💜",
-			url: "https://www.tiktok.com/@eritrajinx/video/7204240403468406021"
+			url: "https://www.tiktok.com/@eritrajinx/video/7205328050429414661"
 		},
 		skins: {
 			skin1: {
