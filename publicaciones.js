1580876131d4471d0ab0bb8d1c0e762689ae358f
xKyru <camc-107@hotmail.com>
2023-03-14 10:56:17 -0600

    updated

--- publicaciones.js
+++ publicaciones.js
@@ -23,7 +23,7 @@ function announcements(announce) {
 		tiktok: {
 			title: "🔥𝐍𝐔𝐄𝐕𝐎 𝐓𝐈𝐊𝐓𝐎𝐊🔥",
 			msg: "Me ayudas mucho al darle like, comentar y compartir 💜",
-			url: "https://www.tiktok.com/@eritrajinx/video/7209077173959855366"
+			url: "https://www.tiktok.com/@eritrajinx/video/7210186842593856774"
 		},
 		skins: {
 			skin1: {
