f1b4ce60d69d777d4734673b3cce399bb857cbf8
xKyru <57547654+xKyru@users.noreply.github.com>
2024-01-18 13:18:28 -0600

    Update publicaciones.js

--- publicaciones.js
+++ publicaciones.js
@@ -2,35 +2,36 @@ announcements(type);
 
 function announcements(announce) {
 	const announceString = announce.toLowerCase().trim();
-	const announceStyles = ["/announce", "/announceblue", "/announcegreen", "/announceorange", "/announcepurple"];
-	let response = `${announceStyles[Math.floor(Math.random() * announceStyles.length)]} `;
+	//const announceStyles = ["/announce", "/announceblue", "/announcegreen", "/announceorange", "/announcepurple"];
+	//let response = `${announceStyles[Math.floor(Math.random() * announceStyles.length)]} `;
+	let response = '';
 	const posts = {
 		instapost: {
 			title: "📷𝐍𝐔𝐄𝐕𝐀 𝐅𝐎𝐓𝐎📷",
 			msg: "Me ayudas mucho al darle like, comentar y compartir 💜",
-			url: "https://www.instagram.com/p/CywzVkZrSL1"
+			url: "https://www.instagram.com/p/C2JCSqgLcCg"
 		},
 		instareel: {
 			title: "👀𝐍𝐔𝐄𝐕𝐎 𝐑𝐄𝐄𝐋👀",
 			msg: "Me ayudas mucho al darle like, comentar y compartir 💜",
-			url: "https://www.instagram.com/p/CsxWQf0AtoC"
+			url: "https://www.instagram.com/p/C1iXg83rbQs"
 		},
 		youtube: {
 			title: "🎥𝐍𝐔𝐄𝐕𝐎 𝐕𝐈𝐃𝐄𝐎🎥",
 			msg: "Me ayudas mucho al darle like, comentar y compartir 💜",
-			url: "https://www.youtube.com/watch?v=B09toQKEAG4"
+			url: "https://www.youtube.com/watch?v=3mESctNXU4E"
 		},
 		tiktok: {
 			title: "🔥𝐍𝐔𝐄𝐕𝐎 𝐓𝐈𝐊𝐓𝐎𝐊🔥",
 			msg: "Me ayudas mucho al darle like, comentar y compartir 💜",
-			url: "https://www.tiktok.com/@eritrajinx/video/7292930583825485062"
+			url: "https://www.tiktok.com/@eritrajinx/video/7324508280011738373"
 		},
 		skins: {
 			skin1: {
-				title: "🚀𝐒𝐎𝐑𝐓𝐄𝐎 𝐀𝐒𝐓𝐑𝐎𝐍𝐀𝐔𝐓 𝐈𝐕𝐄𝐑𝐍🚀",
+				title: "🐝🍯𝐒𝐎𝐑𝐓𝐄𝐎 𝐌𝐈𝐄𝐋❜𝐊𝐎𝐙 🍯🐝",
 				msg: "",
 				/*msg: "Participa por una skin de Zilean. Incluye Campeón + Skin + Chroma #LPP 💜"*/
-				url: "https://twitter.com/EritraJinx/status/1638377540828971008"
+				url: "https://twitter.com/EritraJinx/status/1731704916182237266"
 			},
 		}
 
@@ -77,7 +78,8 @@ function announcements(announce) {
 
 			return response;
 		} else {
-			return `${announceStyles[Math.floor(Math.random() * announceStyles.length)]} ${posts[announceString].title} ${posts[announceString].msg} ${posts[announceString].url}`;
+			//return `${announceStyles[Math.floor(Math.random() * announceStyles.length)]} ${posts[announceString].title} ${posts[announceString].msg} ${posts[announceString].url}`;
+			return `${posts[announceString].title} ${posts[announceString].msg} ${posts[announceString].url}`;
 		}
 	} else {
 		let options = "";
@@ -87,34 +89,3 @@ function announcements(announce) {
 		return `Opciones disponibles: ${options}all`;
 	};
 }
-
-/* !addcom !anuncio $(eval const type = decodeURIComponent(`$(querystring)`); $(urlfetch json https://raw.githubusercontent.com/xKyru/nightbotCommands/main/publicaciones.js)) */
-
-/* 
-𝐀
-𝐁
-𝐂
-𝐃
-𝐄
-𝐅
-𝐆
-𝐇
-𝐈
-𝐉
-𝐊
-𝐋
-𝐌
-𝐍
-𝐎
-𝐏
-𝐐
-𝐑
-𝐒
-𝐓
-𝐔
-𝐕
-𝐖
-𝐗
-𝐘
-𝐙
-*/
